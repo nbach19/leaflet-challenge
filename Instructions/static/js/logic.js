ece74cf0ec5da1bc59153d3fd66dfc2d35a23e91
nbach19 <57469298+nbach19@users.noreply.github.com>
2020-05-03 20:03:54 -0600

    created get request and started to build functions to pull in data

--- Instructions/static/js/logic.js
+++ Instructions/static/js/logic.js
@@ -1,4 +1,17 @@
 // store the json URL as variable jsonUrl
 jsonUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
+// request data from URL using d3.json
+d3.json(jsonUrl, function(data) {
+    //send data.features object to the createFeatures function
+    createFeatures(data.features);
+});
 
+function createFeatures(earthquakeData) {
+
+    // define a function to run fo reach feature in features array
+    // give each feature a popup describing the time and place of earthquake
+    function onEachFEature(feature, layer) {
+        
+    }
+}
